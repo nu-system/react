ffbff4b834614ca542bc20be3509f12c6886e09a
27 <zhengchengzhong@yuewen.com>
2020-06-29 20:52:13 +0800

    fix: publish
    affects: @_nu/react-avatar, @_nu/react-dialog, @_nu/react-img, @_nu/react-select, @_nu/react-toast

--- packages/Grid/es/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-var NuGrid = function NuGrid() {
-  return /*#__PURE__*/React.createElement("div", null, "Grid");
-};
-
-export default NuGrid;
--- packages/Grid/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const NuGrid=()=>{
-  return <div>Grid</div>;
-};
-
-
-export default NuGrid;
